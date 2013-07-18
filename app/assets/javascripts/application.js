3c4e8ea0b491153b7312cd10f0f75f06c3af356f
phil xia <phil.xia@autodesk.com>
2013-07-18 16:11:40 +0800

    fix the gl viewport

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require scenejs/scenejs
 //= require my
--- app/assets/javascripts/my.js
+++ app/assets/javascripts/my.js
@@ -12,7 +12,7 @@ var UILayout = function(){
 			.css("left", handlerleft + $("#handler_vertical").width() - $("#sidebar").width()-2);
 
 		// resize the blocky frame.
-		$("#blocky_frame").css("width", rightwidth - deltaleft-12);
+		//$("#blocky_frame").css("width", rightwidth - deltaleft-12);
 	}
 
 	function _window_resize_handler()
@@ -25,6 +25,7 @@ var UILayout = function(){
 		$("#mainbox").css("width", mainboxWidth);
 		$("#mainbox").css("height", winHeight-105);	
 		$("#leftcontent").css("width", mainboxWidth/2 - 4);
+		$("#leftcontent").css("height", winHeight-105);
 
 		var innerdivheight = $("#leftcontent").height();
 		var innerdivwidth = $("#leftcontent")[0].offsetWidth;
@@ -33,10 +34,33 @@ var UILayout = function(){
 		$("#rightcontent").css("width", mainboxWidth - rightcontentleft - 12)
 			.css("height", innerdivheight)
 			.css("left", rightcontentleft+8);
+		$("#rightcontent").css("height", winHeight-105);
+
+
+		// resize the model viewer.
+		var scene = window.frames["modelviewer_frame"].scene;
+		if(scene && scene != null && false)
+		{
+			var canvas = $('#canvas3d', $('#modelviewer_frame')[0].contentDocument)[0];
+			$('#canvas3d', $('#modelviewer_frame')[0].contentDocument).css("width", innerdivheight - 10);
+			$('#canvas3d', $('#modelviewer_frame')[0].contentDocument).css("height", innerdivwidth - 10);
+			var gl = scene.getGL();
+			if(gl && gl != null)
+				gl.viewport(0, 0, canvas.width, canvas.height);
+			var cameraNode = scene.findNode("theCamera");
+			cameraNode.setOptics({
+								fovy: 25.0,
+								aspect: 1.47,
+								near: 0.10,
+								far: 30000.0
+							});
+
+		}
+		//
 
 		// resize the blocky frame.
-		$("#blocky_frame").css("width", mainboxWidth - rightcontentleft - 30)
-		    .css("height", innerdivheight - 20);
+		// $("#blocky_frame").css("width", mainboxWidth - rightcontentleft - 30)
+		//     .css("height", innerdivheight - 20);
 
 		$("#footer").css("top", winHeight - 50);
 	}
